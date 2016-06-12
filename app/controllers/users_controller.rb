de63ea9fd7d5fca0f2669d4c3392da4288488c81
Mark Lamson <lamson_mark@yahoo.com>
2016-06-12 16:04:49 +0000

    create authentication methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:edit, :update, :show]
   def index
     @users = User.paginate(page: params[:page], per_page: 4)
   end
@@ -18,11 +18,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to concepts_path
@@ -32,7 +31,6 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
     @user_concepts = @user.concepts.paginate(page: params[:page], per_page: 2)
   end
   
@@ -41,4 +39,8 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
 end
